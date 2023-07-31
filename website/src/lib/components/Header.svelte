bba2941f0fff15d0439241640e51635889dcf0c4
Morten Lyng Rosenquist <lyngrosenquistmorten@gmail.com>
2023-07-31 22:48:40 +0200

    Header done

--- website/src/lib/components/Header.svelte
+++ website/src/lib/components/Header.svelte
@@ -1,5 +1,6 @@
 <script>
-	import ThemeToggle from './ThemeToggle.svelte';
+	import HomeIcon from './HomeIcon.svelte';
+import ThemeToggle from './ThemeToggle.svelte';
 
 	const routes = [
 		{ url: '/about', name: 'About' },
@@ -13,22 +14,8 @@
 	id="header"
 	class="fixed left-[48px] top-[48px] bottom-[48px] w-[48px] flex flex-col space-y-12 items-center"
 >
-	<a href="/#intro">
-		<svg
-			class="homeIcon w-6 h-6 text-gray-800 dark:text-white"
-			aria-hidden="true"
-			xmlns="http://www.w3.org/2000/svg"
-			fill="currentColor"
-			viewBox="0 0 483.86 483.86"
-		>
-			<g>
-				<path
-					d="M483.86,112.345h-26.699c-6.952,0-25.143,7.552-25.143,13.981v233.467c0,6.445,18.191,11.729,25.143,11.729h26.699v51.835   H311.052v-51.835h34.57V129.615h-6.016L256.42,423.356h-64.406l-82.11-293.741h-6.219v241.906h34.555v51.835H0v-51.835h19.173   c7.527,0,15.383-5.283,15.383-11.729V126.326c0-6.43-7.855-13.981-15.383-13.981H0V60.504h179.155l58.496,207.342h1.574   l59.073-207.342H483.86V112.345z"
-				/>
-			</g>
-		</svg>
-	</a>
-
+	
+	<HomeIcon></HomeIcon>
 	<nav class="flex flex-col h-full justify-between">
 		<div class="flex flex-col space-y-16">
 			<a href="/#timeline">
@@ -82,7 +69,6 @@
 <ThemeToggle />
 
 <style lang="postcss">
-	
 	p {
 		@apply rotate-[270deg];
 	}
