061322749d934b563b60c868bd97d8106e9f5d51
Morten Lyng Rosenquist <lyngrosenquistmorten@gmail.com>
2023-07-31 15:53:56 +0200

    nav bar

--- website/src/lib/components/Header.svelte
+++ website/src/lib/components/Header.svelte
@@ -15,17 +15,19 @@
 >
 	<a href="/#intro">
 		<svg
-			class="w-6 h-6 text-gray-800 dark:text-white"
+			class="homeIcon w-6 h-6 text-gray-800 dark:text-white"
 			aria-hidden="true"
 			xmlns="http://www.w3.org/2000/svg"
 			fill="currentColor"
-			viewBox="0 0 20 20"
+			viewBox="0 0 483.86 483.86"
 		>
-			<path
-				d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"
-			/>
-		</svg></a
-	>
+			<g>
+				<path
+					d="M483.86,112.345h-26.699c-6.952,0-25.143,7.552-25.143,13.981v233.467c0,6.445,18.191,11.729,25.143,11.729h26.699v51.835   H311.052v-51.835h34.57V129.615h-6.016L256.42,423.356h-64.406l-82.11-293.741h-6.219v241.906h34.555v51.835H0v-51.835h19.173   c7.527,0,15.383-5.283,15.383-11.729V126.326c0-6.43-7.855-13.981-15.383-13.981H0V60.504h179.155l58.496,207.342h1.574   l59.073-207.342H483.86V112.345z"
+				/>
+			</g>
+		</svg>
+	</a>
 
 	<nav class="flex flex-col h-full justify-between">
 		<div class="flex flex-col space-y-16">
@@ -80,24 +82,51 @@
 <ThemeToggle />
 
 <style lang="postcss">
+	
 	p {
-		@apply rotate-[270deg] ;
+		@apply rotate-[270deg];
 	}
 
-	p:hover::after {
+	p::after {
 		content: '';
 		position: absolute;
 		left: 0;
 		top: 45%;
-		height: 4px; /* you can set this as you need */
-		width: 95%;
-		@apply hover:ease-in-out transition duration-1000 bg-[rgb(76,204,204)];
+		height: 4px;
+		width: 100%; /* Full width */
+		background-color: rgb(76, 204, 204);
+		animation: outAnimation 0.3s forwards;
+	}
+
+	p:hover::after {
+		animation: inAnimation 0.3s forwards;
+	}
+
+	@keyframes inAnimation {
+		0% {
+			transform: scaleX(0);
+			transform-origin: left;
+		}
+		100% {
+			transform: scaleX(1);
+			transform-origin: left;
+		}
+	}
+
+	@keyframes outAnimation {
+		0% {
+			transform: scaleX(1);
+			transform-origin: right;
+		}
+		100% {
+			transform: scaleX(0);
+			transform-origin: right;
+		}
 	}
 
 	svg {
 		@apply fill-[rgba(0,0,0,0.6)] hover:fill-[rgb(76,204,204)] hover:ease-in-out transition duration-300;
 	}
 
-	:global(body.dark) .homeIcon {
-	}
+	
 </style>
