d39b4811dfe73a30960e9087df339a30925d501e
Morten Lyng Rosenquist <lyngrosenquistmorten@gmail.com>
2023-08-04 14:50:20 +0200

    added header

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -4,8 +4,16 @@
 </script>
 
 <main>
+	<div class="h-[100vh] flex justify-center items-center flex-col  z-[200]">
 	
+
+	<header class="relative -top-[24px]">
+		<h1>Morten Rosenquist</h1>
+		<h2>Developer</h2>
+	</header>
 	<DisplacementSphere />
+
+</div>
 </main>
 
 <style>
